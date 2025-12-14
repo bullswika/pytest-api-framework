0baf3e0a96ad1a11ec519b9e2355d18527a8594d
bullswika <bullswika@outlook.com>
2025-12-14 06:54:21 +0000

    Split CI - Run API tests automatically and UI tests on demand

---
+++ tests/ui/conftest.py
@@ -0,0 +1,4 @@
+# Jiacheng
+# @Time: 2025/12/14
+# @Email: bullswika@outlook.com
+# @File: conftest.py
---
+++ tests/ui/test_login_ui.py
@@ -0,0 +1,4 @@
+# Jiacheng
+# @Time: 2025/12/14
+# @Email: bullswika@outlook.com
+# @File: test_login_ui.py
